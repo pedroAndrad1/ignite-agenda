9a044bf18747baaad678cfd1a4087eeac6160ee8
Pedro de Andrade <dev.pedroandrade@gmail.com>
2024-07-15 15:57:31 -0300

    feat: Utilizando React Hook Form

--- src/app/(home)/components/UserNameForm/index.tsx
+++ src/app/(home)/components/UserNameForm/index.tsx
@@ -2,14 +2,28 @@
 import { Button, TextInput } from '@pedroandrad1/react'
 import { Form } from './styles'
 import { ArrowRight } from 'phosphor-react'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+
+const UserNameFormSchema = z.object({
+  userName: z.string(),
+})
+type UserNameFormData = z.infer<typeof UserNameFormSchema>
 
 export function UserNameForm() {
+  const { register, handleSubmit } = useForm<UserNameFormData>()
+
+  const handleUserNameForm = (data: UserNameFormData) => {
+    console.log(data)
+  }
+
   return (
-    <Form>
+    <Form as="form" onSubmit={handleSubmit(handleUserNameForm)}>
       <TextInput
-        prefix="ignite-agenda"
+        prefix="ignite.agenda/"
         placeholder="seu-usuario"
         containerProps={{ size: 'sm' }}
+        {...register('userName')}
       />
       <Button size={'sm'} type="submit">
         Reservar
