d7928b84fa7ba3c5dc2c20608ea9e7212cf36b74
Pedro de Andrade <dev.pedroandrade@gmail.com>
2024-07-21 13:42:59 -0300

    feat: custom adapter para o prisma

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,56 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import { PrismaCustomAdapter } from '@/lib/auth/prisma-custom-adpter'
+import NextAuth, { AuthOptions } from 'next-auth'
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
+import { NextRequest } from 'next/server'
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENTE_SECRET as string,
-      authorization: {
-        params: {
-          scope:
-            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+const buildAuthOptions = (req: NextRequest): AuthOptions => {
+  return {
+    adapter: PrismaCustomAdapter(req),
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENTE_SECRET as string,
+        authorization: {
+          params: {
+            scope:
+              'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+          },
         },
+        profile: (profile: GoogleProfile) => {
+          return {
+            id: profile.sub,
+            name: profile.name,
+            username: '',
+            email: profile.email,
+            avatar_url: profile.picture,
+          }
+        },
+      }),
+    ],
+    callbacks: {
+      async signIn({ account }) {
+        if (
+          !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+        ) {
+          return '/registro/conexao-google?error=permissions'
+        }
+
+        return true
       },
-    }),
-  ],
-  callbacks: {
-    async signIn({ account }) {
-      if (
-        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
-      ) {
-        return '/registro/conexao-google?error=permissions'
-      }
-
-      return true
     },
-  },
-})
+  }
+}
 
-export { handler as GET, handler as POST }
+export async function POST(
+  req: NextRequest,
+  routeContext: { params: { nextauth: string[] } },
+) {
+  return NextAuth(req, routeContext, buildAuthOptions(req))
+}
+
+export async function GET(
+  req: NextRequest,
+  routeContext: { params: { nextauth: string[] } },
+) {
+  return NextAuth(req, routeContext, buildAuthOptions(req))
+}
