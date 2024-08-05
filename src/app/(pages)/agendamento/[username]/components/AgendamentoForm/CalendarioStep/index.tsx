6ccddcb95acc39dbc990cb069eb855f5945e8c9e
Pedro de Andrade <dev.pedroandrade@gmail.com>
2024-08-05 12:07:22 -0300

    feat: exibindo horários disponíveis

--- src/app/(pages)/agendamento/[username]/components/AgendamentoForm/CalendarioStep/index.tsx
+++ src/app/(pages)/agendamento/[username]/components/AgendamentoForm/CalendarioStep/index.tsx
@@ -6,16 +6,46 @@ import {
   HorariosPickerHeader,
   HorariosPickerList,
 } from './styles'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import dayjs from 'dayjs'
+import { api } from '@/lib/axios'
+import { usePathname } from 'next/navigation'
+
+interface Disponibilidade {
+  horario: number
+  disponivel: boolean
+}
+
+interface DisponibilidadeResponse {
+  disponibilidades: Disponibilidade[]
+}
 
 export function CalendarioStep() {
   const [selectedDia, setSelectedDia] = useState<Date | null>(null)
+  const [disponibilidades, setDisponibilidades] = useState<Disponibilidade[]>(
+    [],
+  )
   const isDiaSelected = !!selectedDia
+  const pathName = usePathname()
 
   const selectedDiaDaSemana = selectedDia && dayjs(selectedDia).format('dddd')
   const selectedDiaDoMes =
     selectedDia && dayjs(selectedDia).format('DD[ de ]MMMM')
+
+  useEffect(() => {
+    if (!selectedDia) return
+
+    api
+      .get<DisponibilidadeResponse>(
+        `disponibilidade/${pathName.split('/')[2]}`,
+        {
+          params: {
+            date: dayjs(selectedDia).format('YYYY-MM-DD'),
+          },
+        },
+      )
+      .then((res) => setDisponibilidades(res.data.disponibilidades))
+  }, [selectedDia, pathName])
 
   return (
     <CalendarioStepContainer isHorariosPickerOpened={isDiaSelected}>
@@ -26,51 +56,14 @@ export function CalendarioStep() {
             {selectedDiaDaSemana} <span>{selectedDiaDoMes}</span>
           </HorariosPickerHeader>
           <HorariosPickerList>
-            <li>
-              <HorarioPickerItem>08:00h</HorarioPickerItem>
-            </li>
-            <li>
-              <HorarioPickerItem>08:00h</HorarioPickerItem>
-            </li>
-            <li>
-              <HorarioPickerItem>08:00h</HorarioPickerItem>
-            </li>
-            <li>
-              <HorarioPickerItem>08:00h</HorarioPickerItem>
-            </li>
-            <li>
-              <HorarioPickerItem>08:00h</HorarioPickerItem>
-            </li>
-            <li>
-              <HorarioPickerItem>08:00h</HorarioPickerItem>
-            </li>
-            <li>
-              <HorarioPickerItem>08:00h</HorarioPickerItem>
-            </li>
-            <li>
-              <HorarioPickerItem>08:00h</HorarioPickerItem>
-            </li>
-            <li>
-              <HorarioPickerItem>08:00h</HorarioPickerItem>
-            </li>
-            <li>
-              <HorarioPickerItem>08:00h</HorarioPickerItem>
-            </li>
-            <li>
-              <HorarioPickerItem>08:00h</HorarioPickerItem>
-            </li>
-            <li>
-              <HorarioPickerItem>08:00h</HorarioPickerItem>
-            </li>
-            <li>
-              <HorarioPickerItem>08:00h</HorarioPickerItem>
-            </li>
-            <li>
-              <HorarioPickerItem>08:00h</HorarioPickerItem>
-            </li>
-            <li>
-              <HorarioPickerItem>08:00h</HorarioPickerItem>
-            </li>
+            {disponibilidades.map(({ horario, disponivel }) => (
+              <li key={horario}>
+                <HorarioPickerItem disabled={!disponivel}>
+                  {String(horario).padStart(2, '0')}:00h -{' '}
+                  {String(horario + 1).padStart(2, '0')}:00h
+                </HorarioPickerItem>
+              </li>
+            ))}
           </HorariosPickerList>
         </HorariosPickerContainer>
       )}
---
+++ src/app/api/disponibilidade/[username]/route.ts
@@ -0,0 +1,116 @@
+import { prisma } from '@/lib/prisma'
+import { HttpStatusCode } from 'axios'
+import dayjs from 'dayjs'
+import { NextRequest, NextResponse } from 'next/server'
+
+interface PrismaUser {
+  id: string
+  username: string
+  name: string
+  bio: string | null
+  email: string | null
+  avatar_url: string | null
+  created_at: Date
+}
+
+const validateSelectedDia = (selectedDia: dayjs.Dayjs) => {
+  const isDiaPassado = selectedDia.endOf('day').isBefore(new Date())
+  if (isDiaPassado)
+    return {
+      error: true,
+      reponse: NextResponse.json(
+        {},
+        {
+          status: HttpStatusCode.BadRequest,
+          statusText: 'A data é de um dia anterior ao atual.',
+        },
+      ),
+    }
+}
+const validateUser = (user: PrismaUser | null) => {
+  if (!user)
+    return {
+      error: true,
+      response: NextResponse.json(
+        {},
+        {
+          status: HttpStatusCode.NotFound,
+          statusText: 'Usuário não encontrado.',
+        },
+      ),
+    }
+}
+
+const mountDisponibilidadeResponse = async (
+  user: PrismaUser | null,
+  selectedDia: dayjs.Dayjs,
+  inicio: number,
+  fim: number,
+): Promise<
+  {
+    horario: number
+    disponivel: boolean
+  }[]
+> => {
+  const horarios = Array.from({
+    length: fim - inicio,
+  }).map((_, i) => inicio + i)
+
+  const horariosAgendados = await prisma.agendamento.findMany({
+    select: {
+      date: true,
+    },
+    where: {
+      user_id: user?.id,
+      date: {
+        gte: selectedDia.set('hour', inicio).toDate(),
+        lte: selectedDia.set('hour', fim).toDate(),
+      },
+    },
+  })
+
+  return horarios.map((horario) => ({
+    horario,
+    disponivel: !horariosAgendados.some(
+      (horarioAgendado) => horarioAgendado.date.getHours() === horario,
+    ),
+  }))
+}
+
+export async function GET(
+  req: NextRequest,
+  { params: { username } }: { params: { username: string } },
+) {
+  const date = req.nextUrl.searchParams.get('date')
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+  const validateUserResult = validateUser(user)
+  if (validateUserResult?.error) return validateUserResult.response
+
+  const selectedDia = dayjs(String(date))
+  const validateSelectedDiaResult = validateSelectedDia(selectedDia)
+  if (validateSelectedDiaResult?.error) return validateSelectedDiaResult.reponse
+
+  const userDisponibilidade = await prisma.horario.findFirst({
+    where: {
+      user_id: user?.id,
+      dia_da_semana: selectedDia.get('day'),
+    },
+  })
+  if (!userDisponibilidade) return NextResponse.json({ disponibilidades: [] })
+
+  const inicio = userDisponibilidade.inicio_em_minutos / 60
+  const fim = userDisponibilidade.fim_em_minutos / 60
+
+  return NextResponse.json({
+    disponibilidades: await mountDisponibilidadeResponse(
+      user,
+      selectedDia,
+      inicio,
+      fim,
+    ),
+  })
+}
