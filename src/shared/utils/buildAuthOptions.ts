ac82d21cbe11105954486e59c0ed154d152e7b6a
Pedro de Andrade <dev.pedroandrade@gmail.com>
2024-08-08 18:50:13 -0300

    feat: Config para refrest token da google

--- src/shared/utils/buildAuthOptions.ts
+++ src/shared/utils/buildAuthOptions.ts
@@ -11,6 +11,9 @@ export const buildAuthOptions = (): AuthOptions => {
         clientSecret: process.env.GOOGLE_CLIENTE_SECRET as string,
         authorization: {
           params: {
+            prompt: 'consent',
+            access_type: 'offline',
+            response_type: 'code',
             scope:
               'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
           },
